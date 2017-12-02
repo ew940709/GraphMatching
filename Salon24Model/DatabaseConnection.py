5acae24e19e2f40b8ed9cc30f9e259f987c306e8
ew940709 <ew.marczewska@gmail.com>
2017-12-02 20:25:11 +0100

    Generation salon24 subgraphs for time slots

--- Salon24Model/DatabaseConnection.py
+++ Salon24Model/DatabaseConnection.py
@@ -1,10 +1,19 @@
+import datetime
 import networkx as nx
 import psycopg2
+import logging
+
+from Utils import GraphImportExport as export
 
 DB_NAME = "salon24db"
 USER = "sna_user"
 HOST = "localhost"
 PASSWORD = "sna_password"
+DATE_RANGE_START = "2008-01-01"
+DATE_RANGE_END = "2013-07-06"
+DATE_FORMAT = "%Y-%m-%d"
+FORMAT = FORMAT = '%(asctime)-15s %(levelname)s %(message)s'
+logging.basicConfig(format=FORMAT, level=logging.INFO)
 
 
 def connect():
@@ -38,7 +47,7 @@ def add_node(graph, row):
         if graph.has_edge(comment_author, parent_comment):
             graph[comment_author][parent_comment]['weight'] += 1
         else:
-            graph.add_edge(comment_author, parent_comment, weight = 1)
+            graph.add_edge(comment_author, parent_comment, weight=1)
 
 
 def create_graph(cursor):
@@ -52,20 +61,31 @@ def create_graph(cursor):
     return graph
 
 
+def create_graph_for_time_slot(cursor):
+
+    logging.info("Number of rows: {0}".format(cursor.rowcount))
+    row = cursor.fetchone()
+    graph = nx.DiGraph()
+
+    while row is not None:
+        add_node(graph, row)
+        row = cursor.fetchone()
+    return graph
+
+
 def create_social_network(conn):
     graph = None
 
     try:
         cursor = conn.cursor()
         cursor.execute("SELECT c.id, c.author_id, c.date, c.parentcomment_id, c.post_id, p.author_id, "
-                           "(SELECT c1.author_id "
-                            "FROM comments as c1 "
-                            "WHERE c1.id = c.parentcomment_id) as parent_comment_author "
+                       "(SELECT c1.author_id "
+                       "FROM comments as c1 "
+                       "WHERE c1.id = c.parentcomment_id) as parent_comment_author "
                        "FROM comments as c "
                        "JOIN posts as p on p.id = c.post_id ")
 
         graph = create_graph(cursor)
-        cursor.close()
 
     except (Exception, psycopg2.DatabaseError) as error:
         print(error)
@@ -74,3 +94,51 @@ def create_social_network(conn):
             conn.close()
 
     return graph
+
+
+def fetch_comments_by_date(conn, from_date, to_date):
+    try:
+        cursor = conn.cursor()
+        cursor.execute("SELECT c.id, c.author_id, c.date, c.parentcomment_id, c.post_id, p.author_id, "
+                       "(SELECT c1.author_id "
+                       "FROM comments as c1 "
+                       "WHERE c1.id = c.parentcomment_id) as parent_comment_author "
+                       "FROM comments as c "
+                       "JOIN posts as p on p.id = c.post_id "
+                       "WHERE c.date >= '{0}'::date AND  c.date < '{1}'::date".format(from_date, to_date))
+
+        return cursor
+
+    except (Exception, psycopg2.DatabaseError) as error:
+        print(error)
+
+
+def get_next_date_range(last_start):
+    if last_start is None:
+        start_date = datetime.datetime.strptime(DATE_RANGE_START, DATE_FORMAT)
+    else:
+        start_date = last_start + datetime.timedelta(days=3)
+
+    end_date = start_date + datetime.timedelta(days=7)
+    end_date_bounds = datetime.datetime.strptime(DATE_RANGE_END, DATE_FORMAT)
+    if end_date > end_date_bounds:
+        end_date = end_date_bounds
+
+    return start_date, end_date
+
+
+def create_dynamic_graphs_for_salon24():
+    start_date, end_date = get_next_date_range(None)
+
+    conn = connect()
+    i = 1
+    while start_date < datetime.datetime.strptime(DATE_RANGE_END, DATE_FORMAT):
+        cur = fetch_comments_by_date(conn, start_date, end_date)
+        graph = create_graph_for_time_slot(cur)
+        file_path = '..//Salon24_TimeSlots//salon24_{0}_{1}_{2}.json'.format(i, start_date.date(), end_date.date())
+        export.save(graph, file_path)
+        cur.close()
+        i += 1
+        start_date, end_date = get_next_date_range(start_date)
+
+    conn.close()
