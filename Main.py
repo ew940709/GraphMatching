477f8e5e3fc031b8124e96ee6ba0df0f3ceed5b0
Ewa Marczewska <ew.marczewska@gmail.com>
2017-12-18 21:58:15 +0100

    Strong simulation algorithm in basic version implementation

--- Main.py
+++ Main.py
@@ -7,6 +7,7 @@ from Utils import GraphImportExport as importExport
 from Utils import GraphStatistics as statistics
 from AmazonModel import AmazonGraphBuilder as amazonModel
 from Utils import TimeSlotsStatistics as timeSlotStats
+from StrongSimulation import StrongSimulation as strongSimulation
 
 
 
@@ -29,11 +30,24 @@ from Utils import TimeSlotsStatistics as timeSlotStats
 # amazonStatistics.get_statistics(get_basic_stats=False, get_weight=False, get_diameter=False, get_radius=True, get_page_rank=False,
 #                                 get_in_degree=False, get_out_degree=False, get_betweenness_centrality=False)
 
-timeSlotStats.get_stats_for_dynamic_graphs("..\\Salon24_TimeSlots\\", 671)
-
+# timeSlotStats.get_stats_for_dynamic_graphs("..\\Salon24_TimeSlots\\", 671)
+# timeSlotStats.get_percentage_changes_for_slots("salon24_slots.csv")
+#timeSlotStats.get_graph_structure_stats("..\\Salon24_TimeSlots\\", 671)
 # dbConn.create_dynamic_graphs_for_salon24()
 
 
 # importExport.save(G, "test.json")
 # imported_graph = importExport.load("test.json")
 
+number_of_edges = 251
+number_of_nodes = 100
+number_of_query_nodes = 6
+number_of_query_edges = 9
+
+G = nx.fast_gnp_random_graph(number_of_nodes, number_of_edges, directed=True)
+Q = nx.fast_gnp_random_graph(number_of_query_nodes, number_of_query_edges, directed= True)
+query_diameter = nx.diameter(Q)
+
+strongSimulation = strongSimulation.StrongSimulation(Q, G, query_diameter)
+matches = strongSimulation.match()
+print 'kupka'
---
+++ StrongSimulation/StrongSimulation.py
@@ -0,0 +1,73 @@
+import networkx as nx
+
+
+class StrongSimulation:
+
+    def __init__(self, query_graph, graph, query_diameter):
+        self.query_graph = query_graph
+        self.query_diameter = query_diameter
+        self.graph = graph
+
+    def dual_sim(self, ball):
+        sim = {}
+        for vertex in self.query_graph.nodes():
+            if ball.has_node(vertex) and self.graph.has_node(vertex):
+                sim[vertex] = [vertex]
+
+        flag = True
+
+        while flag is True:
+            flag = False
+            for edge in self.query_graph.edges():
+                v = edge[0]
+                v_prim = edge[1]
+                for u in sim[v]:
+                    for u_prim in sim[v_prim]:
+                        if ball.has_edge(u, u_prim) is False:
+                            sim[v].remove(u)
+                            flag = True
+
+        Sw = []
+        for vertex in self.query_graph.nodes():
+            if len(sim[vertex]) == 0:
+                return []
+            Sw.append((vertex, sim[vertex][0]))
+
+        return Sw
+
+    def match(self):
+        result = []
+
+        for vertex in self.graph.nodes():
+            ball = self.get_ball(vertex)
+            Sw = self.dual_sim(ball)
+            Gs = self.extract_max_pg(ball, Sw, vertex)
+            if len(Gs.nodes()) > 0:
+                result.append(Gs)
+
+        return result
+
+    def get_ball(self, vertex):
+        bfs_tree = nx.bfs_tree(self.graph, vertex)  # returns DiGraph
+        subgraph_nodes = []
+        undirected_bfs_tree = bfs_tree.to_undirected()
+        shortest_paths = nx.shortest_path_length(undirected_bfs_tree, vertex);
+
+        for v in shortest_paths:
+            if shortest_paths[v] <= self.query_diameter:
+                subgraph_nodes.append(v)
+
+        ball = self.graph.subgraph(subgraph_nodes)
+
+        return ball
+
+    def extract_max_pg(self, ball, Sw, vertex):
+        if (vertex, vertex) not in Sw:
+            return nx.Graph()
+
+        vertex_list = []
+
+        for t in Sw:
+            vertex_list.append(t[1])
+
+        return ball.subgraph(vertex_list)
